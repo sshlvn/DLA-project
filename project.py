8b9e8621a538fab1f3c3fdb578f62183cd7efa9f
Aleksandra Levina <aleksandra.levin@gmail.com>
2020-12-14 00:26:04 +0300

    deleted arg

--- project.py
+++ project.py
@@ -7,8 +7,12 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/<video_id>')
-def get_json_and_wavs(video_id):
+# @app.route('/<video_id>')
+# def get_json_and_wavs(video_id):
+@app.route('/')
+def get_json_and_wavs():
+    
+    video_id = 'unPU5_vkbx4'
 
     transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
 
