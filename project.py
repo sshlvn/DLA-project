501ee8f16afa530ed43961e609196134ea3501dd
Aleksandra Levina <aleksandra.levin@gmail.com>
2020-12-16 05:39:18 +0300

    added creating transcript if not found

--- project.py
+++ project.py
@@ -29,12 +29,16 @@ def get_json(video_id):
 
     # языки, для которых уже доступен транскрипт
     languages = set()
+    
+    count = 0
 
     for item in transcript_list:
         lang = str(item).split()[0]
         languages.add(lang)
-        if 'auto-generated' in str(item) or 'создано автоматически' in str(
-                item):
+        if count == 0:
+            original_lang = lang
+            count += 1
+        if 'auto-generated' in str(item) or 'создано автоматически' in str(item):
             original_lang = str(item).split()[0]
 
     # если видео русскоязычное, то переводим на англ, с любого другого языка переводим на русский
@@ -51,6 +55,8 @@ def get_json(video_id):
             language)
 
     transcript_list = transcript.fetch()
+    
+    TRANSCRIPT_DICT[video_id] = (transcript_list, language)
 
     # генерятся первые 10
     def generate():
@@ -74,8 +80,6 @@ def get_json(video_id):
     thread = Thread(target=generate)
     thread.start()
 
-    TRANSCRIPT_DICT[video_id] = (transcript_list, language)
-
     # для русского языка отдельно нужно прописать utf-8
     if language == 'ru':
         json_string = json.dumps(transcript_list, ensure_ascii=False)
@@ -90,25 +94,61 @@ def generate_10_wavs(video_id, start_fragment):
     try:
         transcript, lang = TRANSCRIPT_DICT[video_id]
     except:
-        return 'Transcript not found for video ' + video_id
+#         return 'Transcript not found for video ' + video_id
+        try:
+            transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        except:
+            return 'Subtitles not found for video ' + video_id
+
+        # языки, для которых уже доступен транскрипт
+        languages = set()
+
+        count = 0
+
+        for item in transcript_list:
+            lang = str(item).split()[0]
+            languages.add(lang)
+            if count == 0:
+                original_lang = lang
+                count += 1
+            if 'auto-generated' in str(item) or 'создано автоматически' in str(item):
+                original_lang = str(item).split()[0]
+
+        # если видео русскоязычное, то переводим на англ, с любого другого языка переводим на русский
+        if original_lang == 'ru':
+            lang = 'en'
+        else:
+            lang = 'ru'
+
+        # если есть уже сделанный автором транскрипт, то берем его, если нет, то переводим
+        if language in languages:
+            transcript = transcript_list.find_transcript([language])
+        else:
+            transcript = transcript_list.find_transcript([original_lang]).translate(
+                language)
+
+        transcript = transcript.fetch()
 
     def generate():
         for i in range(start_fragment, start_fragment + 10):
-            text = transcript[i]['text']
-            tts = gTTS(text=text, lang=lang, slow=False)
+            try:
+                text = transcript[i]['text']
+                tts = gTTS(text=text, lang=lang, slow=False)
 
-            file_name = video_id + '_' + str(i)
-            tts.save(file_name + '.mp3')
+                file_name = video_id + '_' + str(i)
+                tts.save(file_name + '.mp3')
 
-            # gtts не может сделать нормальный .wav
-            AudioSegment.from_mp3(file_name + '.mp3').export(file_name + '.wav', format='wav')
-            
-            sound = AudioSegment.from_file(file_name + '.wav')
+                # gtts не может сделать нормальный .wav
+                AudioSegment.from_mp3(file_name + '.mp3').export(file_name + '.wav', format='wav')
 
-            # изменение скорости
-            sound_with_altered_frame_rate = sound._spawn(sound.raw_data, overrides={"frame_rate": int(sound.frame_rate * 1.25)})
-            new_sound = sound_with_altered_frame_rate.set_frame_rate(sound.frame_rate)
-            new_sound.export(file_name + '.wav', format='wav')
+                sound = AudioSegment.from_file(file_name + '.wav')
+
+                # изменение скорости
+                sound_with_altered_frame_rate = sound._spawn(sound.raw_data, overrides={"frame_rate": int(sound.frame_rate * 1.25)})
+                new_sound = sound_with_altered_frame_rate.set_frame_rate(sound.frame_rate)
+                new_sound.export(file_name + '.wav', format='wav')
+            except:
+                return "I generated less than 10 wavs but it's OK"
 
     thread = Thread(target=generate)
     thread.start()
