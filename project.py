0f632a8c7b3816f198bad6537d40b23d2c8b4619
Aleksandra Levina <aleksandra.levin@gmail.com>
2020-12-17 01:34:45 +0300

    Update project.py

--- project.py
+++ project.py
@@ -8,7 +8,7 @@ import requests
 app = Flask(__name__)
 
 os.system('wget http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb')
-os.system('sudo dpkg -i libpng12.deb')
+os.system('dpkg -i libpng12.deb')
 
 def synthesize(text, language):
     folder_id = 'b1ggo3uv5jlc4dgbi4fm'
