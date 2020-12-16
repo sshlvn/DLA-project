7d4960d5dfddf490750949876fcd79117c068b41
Aleksandra Levina <aleksandra.levin@gmail.com>
2020-12-16 05:47:46 +0300

    return to getting json everytime

--- project.py
+++ project.py
@@ -8,20 +8,7 @@ from pydub import AudioSegment
 
 app = Flask(__name__)
 
-# сохраняем video_id: (transcript, language)
-TRANSCRIPT_DICT = dict()
-
-@app.route('/json/<video_id>')
-def get_json(video_id):
-    # если транскрипт уже делали
-    if video_id in TRANSCRIPT_DICT:
-        transcript, lang = TRANSCRIPT_DICT[video_id]
-        if lang == 'ru':
-            json_string = json.dumps(transcript, ensure_ascii=False)
-            return Response(json_string,
-                            content_type="application/json; charset=utf-8")
-        return jsonify(transcript)
-
+def get_transcript(video_id):
     try:
         transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
     except:
@@ -56,29 +43,13 @@ def get_json(video_id):
 
     transcript_list = transcript.fetch()
     
-    TRANSCRIPT_DICT[video_id] = (transcript_list, language)
+    return transcript_list, language
+    
 
-    # генерятся первые 10
-    def generate():
-        for i in range(10):
-            text = transcript_list[i]['text']
-            tts = gTTS(text=text, lang=language, slow=False)
-
-            file_name = video_id + '_' + str(i)
-            tts.save(file_name + '.mp3')
-
-            # gtts не может сделать нормальный .wav
-            AudioSegment.from_mp3(file_name + '.mp3').export(file_name + '.wav', format='wav')
-            
-            sound = AudioSegment.from_file(file_name + '.wav')
-
-            # изменение скорости
-            sound_with_altered_frame_rate = sound._spawn(sound.raw_data, overrides={"frame_rate": int(sound.frame_rate * 1.25)})
-            new_sound = sound_with_altered_frame_rate.set_frame_rate(sound.frame_rate)
-            new_sound.export(file_name + '.wav', format='wav')
-
-    thread = Thread(target=generate)
-    thread.start()
+@app.route('/json/<video_id>')
+def get_json(video_id):
+    
+    transcript_list, language = get_transcript(video_id)
 
     # для русского языка отдельно нужно прописать utf-8
     if language == 'ru':
@@ -91,43 +62,8 @@ def get_json(video_id):
 
 @app.route('/10wavs/<video_id>&&<int:start_fragment>')
 def generate_10_wavs(video_id, start_fragment):
-    try:
-        transcript, lang = TRANSCRIPT_DICT[video_id]
-    except:
-#         return 'Transcript not found for video ' + video_id
-        try:
-            transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-        except:
-            return 'Subtitles not found for video ' + video_id
-
-        # языки, для которых уже доступен транскрипт
-        languages = set()
-
-        count = 0
-
-        for item in transcript_list:
-            lang = str(item).split()[0]
-            languages.add(lang)
-            if count == 0:
-                original_lang = lang
-                count += 1
-            if 'auto-generated' in str(item) or 'создано автоматически' in str(item):
-                original_lang = str(item).split()[0]
-
-        # если видео русскоязычное, то переводим на англ, с любого другого языка переводим на русский
-        if original_lang == 'ru':
-            lang = 'en'
-        else:
-            lang = 'ru'
-
-        # если есть уже сделанный автором транскрипт, то берем его, если нет, то переводим
-        if language in languages:
-            transcript = transcript_list.find_transcript([language])
-        else:
-            transcript = transcript_list.find_transcript([original_lang]).translate(
-                language)
-
-        transcript = transcript.fetch()
+    
+    transcript, lang = get_transcript(video_id)
 
     def generate():
         for i in range(start_fragment, start_fragment + 10):
