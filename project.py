990315e4049a582837547d48886684fde894b582
Aleksandra Levina <aleksandra.levin@gmail.com>
2020-12-17 00:58:59 +0300

    apt-get install sox

--- project.py
+++ project.py
@@ -7,9 +7,7 @@ import requests
 
 app = Flask(__name__)
 
-os.system('wget -q -O /tmp/libpng12.deb http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb \
-  && dpkg -i /tmp/libpng12.deb \
-  && rm /tmp/libpng12.deb')
+os.system('apt-get install sox')
 
 def synthesize(text, language):
     folder_id = 'b1ggo3uv5jlc4dgbi4fm'
