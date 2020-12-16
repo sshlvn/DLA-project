2f9c85b19c91cdb344e83edc8be29ba5aba99b61
Aleksandra Levina <aleksandra.levin@gmail.com>
2020-12-17 01:22:04 +0300

    try to deal with libpng

--- project.py
+++ project.py
@@ -7,7 +7,8 @@ import requests
 
 app = Flask(__name__)
 
-# os.system('apt-get install sox')
+os.system('wget http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb')
+os.system('sudo dpkg -i libpng12.deb')
 
 def synthesize(text, language):
     folder_id = 'b1ggo3uv5jlc4dgbi4fm'
